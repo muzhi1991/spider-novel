4b9acb970b066afdbdce5748f83a213935eb7be9
limuzhi <limuzhi@baidu.com>
2019-03-09 15:06:13 +0800

    add for one drive gz cp

--- bookstore_catalog_manager.py
+++ bookstore_catalog_manager.py
@@ -4,18 +4,21 @@ import os
 import json
 import logging
 
+
 class BookCatalogManager:
     catalog = OrderedDict()
     done = set()
+    onedrive = set()
     catalog_file = None
     done_file = None
+    onedrive_file = None
     store_path = None
 
     urls = OrderedDict()
 
-
     def __enter__(self):
         return self
+
     def __exit__(self):
         try:
             if BookCatalogManager.catalog_file is not None:
@@ -25,12 +28,16 @@ class BookCatalogManager:
         except Exception as e:
             logging.exception("close file error")
 
-
     @staticmethod
     def load_catalog_info(bookstore_dir_path):
         BookCatalogManager.store_path = bookstore_dir_path
         catalog_path = os.path.join(bookstore_dir_path, "__CATALOG__")
         done_path = os.path.join(bookstore_dir_path, "__DONE__")
+        onedrive_path = os.path.join(bookstore_dir_path, "__ONEDRIVE__")
+
+        # Opens a file for appending. The file pointer is at the end of the file if the file exists.
+        # That is, the file is in the append mode. If the file does not exist, it creates a new file for writing.
+
         # rel_book_path = os.path.relpath(book_dir_path, __BOOK_STORE_PATH__)
         # logging.debug(catalog_path)
         # os.path.exists(catalog_path) or Path(catalog_path).touch()
@@ -38,8 +45,10 @@ class BookCatalogManager:
         # The file pointer will be at the beginning of the file.
         Path(catalog_path).touch()
         Path(done_path).touch()
+        Path(onedrive_path).touch()
         BookCatalogManager.catalog_file = open(catalog_path, "r+", encoding='utf8')
         BookCatalogManager.done_file = open(done_path, "r+", encoding='utf8')
+        BookCatalogManager.onedrive_file = open(onedrive_path, 'r+', encoding='utf8')
         for line in BookCatalogManager.catalog_file:
             if line:
                 arr = line.strip().split("\t")  # 去除\n
@@ -73,6 +82,14 @@ class BookCatalogManager:
                     book_rel_path = arr[0]
                 if book_rel_path:
                     BookCatalogManager.done.add(book_rel_path)
+        for line in BookCatalogManager.onedrive_file:
+            if line:
+                arr = line.split()
+                book_rel_path = None
+                if len(arr) == 1:
+                    book_rel_path = arr[0]
+                if book_rel_path:
+                    BookCatalogManager.onedrive.add(book_rel_path)
 
     @staticmethod
     def add_book_to_catalog(book_dir_path, book_url):
@@ -96,19 +113,19 @@ class BookCatalogManager:
         return False
 
     @staticmethod
-    def load_manifest_detail_by_url(book_path):
+    def load_manifest_detail_by_url(book_url):
         BookCatalogManager.load_manifest_detail(
-            os.path.join(BookCatalogManager.store_path, BookCatalogManager.urls[book_path]))
+            os.path.join(BookCatalogManager.store_path, BookCatalogManager.urls[book_url]))
 
     @staticmethod
     def load_manifest_detail(book_path):
         path = os.path.join(book_path, "__MANIFEST_DETAIL__")
         try:
-                with open(path, encoding='utf-8') as f:
-                    return json.load(f)
+            with open(path, encoding='utf-8') as f:
+                return json.load(f)
         except Exception as e:
-                print(e)
-                return None
+            print(e)
+            return None
         return None
 
     @staticmethod
@@ -130,6 +147,42 @@ class BookCatalogManager:
         return False
 
     @staticmethod
+    def append_to_book_onedrive_file(book_dir_path):
+        """
+        加入onedriver发送
+        :param book_info:
+        :return:
+        """
+        # fixme main thread
+        rel_book_path = os.path.relpath(book_dir_path, BookCatalogManager.store_path)
+        BookCatalogManager.onedrive_file.write(str(rel_book_path) + "\n")
+        BookCatalogManager.onedrive.add(rel_book_path)
+
+    @staticmethod
+    def is_status_onedrive_byurl(book_url):
+        """
+        是否已经onedriver发送
+        :param book_url:
+        :return:
+        """
+        # fixme main thread
+        if book_url in BookCatalogManager.urls:
+            if BookCatalogManager.urls[book_url] in BookCatalogManager.onedrive:
+                return True
+        return False
+
+    @staticmethod
+    def is_status_onedrive(book_path):
+        """
+        是否已经onedriver发送
+        :param book_path:
+        :return:
+        """
+        if book_path in BookCatalogManager.onedrive:
+            return True
+        return False
+
+    @staticmethod
     def refresh_new_catalog(bookstore_dir_path):
         for k, v in BookCatalogManager.catalog.items():
             print(k, v)
@@ -146,6 +199,38 @@ class BookCatalogManager:
                 else:
                     print("error:", k, v)
         pass
+
+    @staticmethod
+    def get_all_stat_cnt():
+        # todo 使用walk实现
+        logging.info("开始{}下载数据统计数据".format(BookCatalogManager.store_path))
+        catalog_path = os.path.join(BookCatalogManager.store_path, "__CATALOG__")
+        sum_book = 0
+        sum_chapter = 0
+        with open(catalog_path, "r", encoding='utf8') as f:
+            for line in f:
+                if line:
+                    arr = line.strip().split("\t")  # 去除\n
+                    book_rel_path = None
+                    book_url = None
+                    if len(arr) == 2:
+                        book_rel_path = arr[0]
+                        book_url = arr[1]
+                        # BookCatalogManager.catalog.add(book_rel_path)
+                        book_detail = BookCatalogManager.load_manifest_detail(
+                            os.path.join(BookCatalogManager.store_path, book_rel_path))
+                        sum_book += 1
+                        sum_chapter += len(book_detail["chapter_list"])
+        # 减去booklist.json __CATALOG__ __DONE__三个
+        dir_list = next(os.walk(BookCatalogManager.store_path))[1]
+        # 各个类别的下载的文件数目
+        total_down_file_cnt = [sum([len(files) for r, d, files in
+                                    os.walk(os.path.join(BookCatalogManager.store_path, dir_item))])
+                               for
+                               dir_item in dir_list]
+
+        downloaded_chapter = sum(total_down_file_cnt) - sum_book  # 每个书有个manifest文件
+        return sum_book, sum_chapter, downloaded_chapter
 
 
 if __name__ == "__main__":
--- status_monitor.py
+++ status_monitor.py
@@ -11,56 +11,61 @@ class StatusMonitor:
     flag = True
 
     @staticmethod
-    def set_monitor(key, total, unit, initial_val=0, desc=None):
+    def set_monitor(key, total, unit, initial_val=0, desc=None, callback=None, progress=True):
         if not StatusMonitor.flag: return
         pos = 0
-        if len(StatusMonitor.monitor_pos_h) != 0:
-            pos = heapq.heappop(StatusMonitor.monitor_pos_h)
-        else:
-            pos = StatusMonitor.total_pos
+        t = None
+        if progress and StatusMonitor.flag:
+            if len(StatusMonitor.monitor_pos_h) != 0:
+                pos = heapq.heappop(StatusMonitor.monitor_pos_h)
+            else:
+                pos = StatusMonitor.total_pos
             StatusMonitor.total_pos += 1
+            t = tqdm(total=total, unit=unit, file=sys.stdout, initial=initial_val,
+                     position=pos)
+            t.update(0)
+            t.set_description(desc)
+            if desc is None:
+                desc = key
 
-        t = tqdm(total=total, unit=unit, file=sys.stdout, initial=initial_val,
-                 position=pos)
-        t.update(0)
-        if desc is None:
-            desc = key
-        t.set_description(desc)
-        StatusMonitor.status[key] = {"total": total, "unit": unit, "val": initial_val, "tqdm": t,
-                                     "desc": desc, "pos": pos}
+        StatusMonitor.status[key] = {"total": total, "unit": unit, "val": initial_val,
+                                     "tqdm": t,
+                                     "desc": desc, "pos": pos, "callback": callback,
+                                     "progress": progress}
 
     @staticmethod
     def del_monitor(key):
-        if not StatusMonitor.flag: return
         if key in StatusMonitor.status:
             r = StatusMonitor.status.pop(key)
-            r["tqdm"].close()
-            heapq.heappush(StatusMonitor.monitor_pos_h, r["pos"])
-            if "book" in StatusMonitor.status:
-                StatusMonitor.update_monitor("book")
+            if StatusMonitor.total_pos and r["progress"]:
+                r["tqdm"].close()
+                heapq.heappush(StatusMonitor.monitor_pos_h, r["pos"])
+            if "callback" in r and r["callback"] is not None:
+                r["callback"](key)
+            if "sum" in StatusMonitor.status:
+                StatusMonitor.update_monitor("sum")
 
     @staticmethod
     def update_monitor(key, increment=1):
-        if not StatusMonitor.flag: return
         if key in StatusMonitor.status:
             item = StatusMonitor.status[key]
             # 更新位置
-            if len(StatusMonitor.monitor_pos_h) != 0:
-                pos = item["pos"]
-                if pos > StatusMonitor.monitor_pos_h[0]:
-                    pos = heapq.heapreplace(StatusMonitor.monitor_pos_h, pos)
-                    item["pos"] = pos
-                    item["tqdm"].clear()
-                    item["tqdm"].close()
-                    item["tqdm"] = tqdm(total=item["total"], unit=item["unit"], file=sys.stdout,
-                                    initial=item["val"],
-                                    position=item["pos"])
-
-                pass
+            if StatusMonitor.total_pos and item["progress"]:
+                if len(StatusMonitor.monitor_pos_h) != 0:
+                    pos = item["pos"]
+                    if pos > StatusMonitor.monitor_pos_h[0]:
+                        pos = heapq.heapreplace(StatusMonitor.monitor_pos_h, pos)
+                        item["pos"] = pos
+                        item["tqdm"].clear()
+                        item["tqdm"].close()
+                        item["tqdm"] = tqdm(total=item["total"], unit=item["unit"], file=sys.stdout,
+                                            initial=item["val"],
+                                            position=item["pos"])
+                item["tqdm"].update(increment)
+                item["tqdm"].set_description(item["desc"])
 
             item["val"] += increment
-            item["tqdm"].update(increment)
-            item["tqdm"].set_description(item["desc"])
             # 100%后自动删除
             if item["val"] == item["total"]:
                 StatusMonitor.del_monitor(key)
+
